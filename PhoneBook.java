1d36f2ada96bea68d6e0c0ea1c15b66cf600517d
Anna Sapr <annsapronova5492818@mail.ru>
2024-05-04 13:58:57 +0300

    add func for delit

--- PhoneBook.java
+++ PhoneBook.java
@@ -1,3 +1,5 @@
+package PhoneBook;
+
 import java.util.*;
 
 public class PhoneBook {
@@ -10,11 +12,13 @@ public class PhoneBook {
             System.out.println("Выберите действие:");
             System.out.println("1. Добавить контакт");
             System.out.println("2. Вывести список всех телефонов");
-            System.out.println("3. Выйти");
+            System.out.println("3. Удалить номер телефона");
+            System.out.println("4. Удалить контакт");
+            System.out.println("5. Выйти");
 
             if (scanner.hasNextInt()) {
                 int choice = scanner.nextInt();
-                scanner.nextLine(); // очистка буфера после nextInt()
+                scanner.nextLine();
 
                 switch (choice) {
                     case 1:
@@ -32,14 +36,20 @@ public class PhoneBook {
                         displayPhoneBook(phoneBook);
                         break;
                     case 3:
+                        removePhoneNumber(phoneBook);
+                        break;
+                    case 4:
+                        removeContact(phoneBook);
+                        break;
+                    case 5:
                         running = false;
                         break;
                     default:
-                        System.out.println("Неверный выбор. Пожалуйста, выберите от 1 до 3.");
+                        System.out.println("Неверный выбор. Пожалуйста, выберите от 1 до 5.");
                 }
             } else {
                 System.out.println("Неверный ввод. Пожалуйста, введите цифру.");
-                scanner.nextLine();
+                scanner.nextLine(); // очистка буфера
             }
         }
 
@@ -64,6 +74,39 @@ public class PhoneBook {
         System.out.println("Список всех телефонов:");
         for (Map.Entry<String, Set<String>> entry : phoneBook.entrySet()) {
             System.out.println("Имя: " + entry.getKey() + ", Телефоны: " + entry.getValue());
+        }
+    }
+
+    // Метод для удаления номера телефона
+    public static void removePhoneNumber(Map<String, Set<String>> phoneBook) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите имя для удаления номера телефона:");
+        String name = scanner.nextLine();
+        if (phoneBook.containsKey(name)) {
+            Set<String> phones = phoneBook.get(name);
+            System.out.println("Введите номер телефона для удаления:");
+            String phone = scanner.nextLine();
+            if (phones.contains(phone)) {
+                phones.remove(phone);
+                System.out.println("Номер телефона " + phone + " удален для контакта " + name);
+            } else {
+                System.out.println("Номер телефона не найден для контакта " + name);
+            }
+        } else {
+            System.out.println("Контакт с именем " + name + " не найден");
+        }
+    }
+
+    // Метод для удаления контакта
+    public static void removeContact(Map<String, Set<String>> phoneBook) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите имя контакта для удаления:");
+        String name = scanner.nextLine();
+        if (phoneBook.containsKey(name)) {
+            phoneBook.remove(name);
+            System.out.println("Контакт " + name + " удален из телефонной книги");
+        } else {
+            System.out.println("Контакт с именем " + name + " не найден");
         }
     }
 }
